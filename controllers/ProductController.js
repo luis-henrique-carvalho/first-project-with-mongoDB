d953dd02a62b91cf4ffd415abc342323c0b452ec
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2023-02-21 16:38:37 -0300

    :sparkles: feat: get individual data

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,23 +1,28 @@
-const Product = require('../models/Product')
+const Product = require("../models/Product");
 
 module.exports = class ProductController {
-  static showProducts(req, res) {
-    res.render('products/all')
+  static async showProducts(req, res) {
+    const products = await Product.getProducts();
+
+    console.log(products);
+
+    res.render("products/all", { products });
   }
 
   static createProducts(req, res) {
-    res.render('products/create')
+    res.render("products/create");
   }
 
   static createProductsPost(req, res) {
-    const name= req.body.name
-    const price= req.body.price
-    const description= req.body.description
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+    const image = req.body.image;
 
-    const product = new Product(name, price, description)
+    const product = new Product(name, price, description, image);
 
-    product.save()
+    product.save();
 
-    res.redirect('/')
+    res.redirect("/");
   }
-}
+};
