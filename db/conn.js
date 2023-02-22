d0efa56d22cf49908530af04bdc88c1de9b82f54
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2023-02-22 14:02:15 -0300

    :spakles: feat: using mongoose

--- db/conn.js
+++ db/conn.js
@@ -1,28 +1,29 @@
-// const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-// async function main() {
-//   await mongoose.connect("mongodb://localhost:2707/testemongoose");
-//   console.log("Conectou ao MongoDB como Mongoose!");
-// }
-
-// main().catch((err)=> console.log(err))
-
-// module.exports = mongoose
-
-const { MongoClient } = require("mongodb");
-// Connection URI
-const url = "mongodb://localhost:27017/testemongodb";
-// Create a new MongoClient
-const client = new MongoClient(url);
-async function run() {
-  try {
-    await client.connect();
-    console.log("Conectado ao MongoDB!");
-  } catch (err) {
-    console.log(err);
-  }
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/testemongoose')
+  console.log('Conectou com Mongoose!')
 }
 
-run();
+main().catch((err) => console.log(err))
 
-module.exports = client;
+module.exports = mongoose
+
+
+// const { MongoClient } = require("mongodb");
+// // Connection URI
+// const url = "mongodb://localhost:27017/testemongodb";
+// // Create a new MongoClient
+// const client = new MongoClient(url);
+// async function run() {
+//   try {
+//     await client.connect();
+//     console.log("Conectado ao MongoDB!");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// run();
+
+// module.exports = client;
--- models/Product.js
+++ models/Product.js
@@ -1,57 +1,26 @@
-const { ObjectId } = require('mongodb')
-const conn = require('../db/conn')
+const mongoose = require('../db/conn')
+const { Schema } = mongoose
 
-class Product {
-  constructor(name, price, description, image) {
-    this.name = name
-    this.price = price
-    this.description = description
-    this.image = image
-  }
-
-  save() {
-    const product = conn.db().collection('products').insertOne({
-      name: this.name,
-      price: this.price,
-      description: this.description,
-      image: this.image,
-    })
-
-    return product
-  }
-
-  static getProducts() {
-    const products = conn.db().collection('products').find().toArray()
-
-    return products
-  }
-
-  static async getProductById(id) {
-    const product = await conn
-      .db()
-      .collection('products')
-      .findOne({ _id: ObjectId(id) })
-
-    return product
-  }
-
-  static async removeProduct(id) {
-    await conn
-      .db()
-      .collection('products')
-      .deleteOne({ _id: ObjectId(id) })
-
-    return
-  }
-
-  updateProduct(id) {
-    conn
-      .db()
-      .collection('products')
-      .updateOne({ _id: ObjectId(id) }, { $set: this })
-
-    return
-  }
-}
+const Product = mongoose.model(
+  'Products',
+  new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  }),
+)
 
 module.exports = Product
